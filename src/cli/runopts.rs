e9111850cc9ea5f42814bb1a4711adbd111cabee
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2025-02-18 12:01:53 -0800

    feat: added a parallel runtime for fastq with paraseq

--- src/cli/runopts.rs
+++ src/cli/runopts.rs
@@ -4,5 +4,14 @@ use clap::Parser;
 #[clap(next_help_heading = "RUN OPTIONS")]
 pub struct RunOptions {
     #[clap(short = 'T', long, default_value = "1")]
-    pub n_threads: usize,
+    n_threads: usize,
+}
+impl RunOptions {
+    pub fn n_threads(&self) -> usize {
+        if self.n_threads == 0 {
+            num_cpus::get()
+        } else {
+            self.n_threads.min(num_cpus::get())
+        }
+    }
 }
